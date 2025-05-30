cc0230b8ccdc0ee0d61570cf965803f17fdd392d
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-05-30 11:29:18 -0300

    Finalizando o componente de input

--- src/components/Inputs/index.tsx
+++ src/components/Inputs/index.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { InputHTMLAttributes, useState } from "react"
 import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa'
 
@@ -14,25 +16,27 @@ export const InputComponent = ({ isPassword, ...rest }: InputProps) => {
     }
 
     if (isPassword) {
-        <div className="w-[100%] h-[54px] border rounded-xl flex items-center justify-center overflow-hidden">
-            <input
-                type={type}
-                className="w-[100%] h-[100%] pl-2.5 text-[16px]"
-            />
+        return (
+            <div className="w-[100%] h-[54px] mt-2 mb-2 border rounded-[8px] flex items-center justify-center overflow-hidden relative">
+                <input
+                    type={type}
+                    className="w-[100%] h-[100%] pl-2.5 text-[16px]"
+                />
 
-            <button
-                className="h-[100%] p-2 flex items-center justify-center absolute top-0 right-3"
-                onClick={verSenha}
-            >
-                {
-                    type == "password" ? <FaRegEyeSlash size={35} /> : <FaRegEye size={35} />
-                }
-            </button>
-        </div>
+                <button
+                    className="h-[100%] p-2 flex items-center justify-center absolute top-0 right-1"
+                    onClick={verSenha}
+                >
+                    {
+                        type == "password" ? <FaRegEyeSlash size={35} /> : <FaRegEye size={35} />
+                    }
+                </button>
+            </div>
+        )
     }
 
     return (
-        <div className="w-[100%] h-[54px] border rounded-xl flex items-center justify-center overflow-hidden">
+        <div className="w-[100%] h-[54px] mt-2 mb-2 border rounded-[8px] flex items-center justify-center overflow-hidden">
             <input
                 type="text"
                 className="w-[100%] h-[100%] pl-2.5 text-[16px]"
