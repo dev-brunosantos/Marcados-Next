4636533058ddaff4e800c25ac6e431c8dcad6eb3
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-05-30 11:39:26 -0300

    Adicionando os componentes na tela de login

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,16 +1,34 @@
+import { BtnComponent } from "@/src/components/Buttons";
 import { InputComponent } from "@/src/components/Inputs";
 import { Tela, TelaContainer } from "@/src/components/Tela";
+import Link from "next/link";
 
 export default function Login() {
     return (
         <Tela>
             <TelaContainer>
+                <h1 className="text-[3rem] font-bold">
+                    {/* Marcados */}
+                </h1>
+            </TelaContainer>
+
+            <TelaContainer>
                 <InputComponent
                     isPassword={false}
+                    placeholder="Digite seu e-mail"
                 />
                 <InputComponent
                     isPassword={true}
+                    placeholder="Digite sua senha"
                 />
+            </TelaContainer>
+
+            <TelaContainer>
+                <BtnComponent
+                    titulo="Entrar"
+                />
+                
+                <Link href="/">Não tem conta? Cadastre-se aqui</Link>
             </TelaContainer>
         </Tela>
     )
