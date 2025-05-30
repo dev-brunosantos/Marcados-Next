6ffc78e229a8319b03417cc0c2f4bb27d8dac4d5
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-05-30 14:59:03 -0300

    Melhorando a logica da tela de cadastro de usuário

--- src/app/(pages)/cadastro/usuario/page.tsx
+++ src/app/(pages)/cadastro/usuario/page.tsx
@@ -1,9 +1,44 @@
+"use client"
+
 import { BtnComponent } from "@/src/components/Buttons";
-import { InputComponent } from "@/src/components/Inputs";
 import { Tela, TelaContainer } from "@/src/components/Tela";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { SelectNaipe } from "./SelectComponent";
+import { Formulario } from "./Formulario";
 
-export default function CadastroUsuario() {
+interface CargoProps {
+    id: number;
+    cargo: string;
+    dtCriacao: string;
+    dtAtualizacao: string;
+}
+
+export default function CadastroUsuario(naipe: string) {
+
+    const [cargos, setCargos] = useState<CargoProps[]>([])
+    const [cargo, setCargo] = useState('')
+
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [confirmacaoSenha, setConfirmacaoSenha] = useState('')
+
+    useEffect(() => {
+        async function buscaDados() {
+            var response = await fetch('http://localhost:9000/cargos')
+            var dados = await response.json()
+
+            if (!dados) {
+                throw new Error("Erro")
+            }
+            console.log(dados)
+            return setCargos(dados)
+        }
+
+        buscaDados()
+    }, [])
+
     return (
         <Tela>
             <TelaContainer>
@@ -13,29 +48,53 @@ export default function CadastroUsuario() {
             </TelaContainer>
 
             <TelaContainer>
-                <InputComponent
-                    isPassword={false}
-                    placeholder="Digite seu nome"
-                />
-                <InputComponent
-                    isPassword={false}
-                    placeholder="Digite seu e-mail"
-                />
-                <InputComponent
-                    isPassword={true}
-                    placeholder="Digite sua senha"
-                />
-                <InputComponent
-                    isPassword={true}
-                    placeholder="Confirme sua senha"
+                <Formulario
+                    nome={nome} setNome={setNome}
+                    email={email} setEmail={setEmail}
+                    senha={senha} setSenha={setSenha}
+                    confirmacaoSenha={confirmacaoSenha}
+                    setConfirmacaoSenha={setConfirmacaoSenha}
                 />
             </TelaContainer>
 
             <TelaContainer>
+                <select value={cargo} onChange={(e) => setCargo(e.target.value)}>
+                    <option value="">Escolha seu cargo</option>
+                    {
+                        cargos.map(card => (
+                            <option key={card.id} value={card.cargo}>
+                                {card.cargo}
+                            </option>
+                        ))
+                    }
+                </select>
+                {
+                    cargo == "Vocal" && (
+                        <SelectNaipe naipe="Vozes" placeholder="Escolha seu Naipe" />
+                    )
+                }
+                {
+                    cargo == "Musico" && (
+                        <SelectNaipe naipe="Instrumentos" placeholder="Escolha seu instrumento" />
+                    )
+                }
+            </TelaContainer>
+
+            <TelaContainer>
                 <BtnComponent
-                    titulo="Entrar"
+                    titulo="Cadastrar"
+                    onClick={() => {
+                        if(nome.trim() == "" || email.trim() == "" || senha.trim() == "") {
+                            return alert("Todos os campos devem ser preenchidos")
+                        }
+
+                        if (confirmacaoSenha != senha) {
+                            return alert("As senhas não são iguais.")
+                        }
+                        console.log(`${nome}, ${email}, ${senha}`)
+                    }}
                 />
-                
+
                 <Link href="/login">Já tem conta? Faça login aqui</Link>
             </TelaContainer>
         </Tela>
