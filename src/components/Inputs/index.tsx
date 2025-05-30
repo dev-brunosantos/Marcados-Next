06b47a9e281423d5d91371b3ae15efb75f7ee432
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-05-30 11:17:59 -0300

    Criando o componente Input

--- src/components/Inputs/index.tsx
+++ src/components/Inputs/index.tsx
@@ -1,13 +1,42 @@
-import { InputHTMLAttributes } from "react"
+import { InputHTMLAttributes, useState } from "react"
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     isPassword: boolean;
 }
 
-export const InputComponent = ({ isPassword, ...rest }:InputProps) => {
-    return(
-        <div className="w-[100%] h-[54px] border rounded-xl flex items-center justify-center">
-            <input type="text" />
+export const InputComponent = ({ isPassword, ...rest }: InputProps) => {
+
+    const [type, setType] = useState('password')
+
+    const verSenha = () => {
+        type == "password" ? setType("text") : setType("password")
+    }
+
+    if (isPassword) {
+        <div className="w-[100%] h-[54px] border rounded-xl flex items-center justify-center overflow-hidden">
+            <input
+                type={type}
+                className="w-[100%] h-[100%] pl-2.5 text-[16px]"
+            />
+
+            <button
+                className="h-[100%] p-2 flex items-center justify-center absolute top-0 right-3"
+                onClick={verSenha}
+            >
+                {
+                    type == "password" ? <FaRegEyeSlash size={35} /> : <FaRegEye size={35} />
+                }
+            </button>
+        </div>
+    }
+
+    return (
+        <div className="w-[100%] h-[54px] border rounded-xl flex items-center justify-center overflow-hidden">
+            <input
+                type="text"
+                className="w-[100%] h-[100%] pl-2.5 text-[16px]"
+            />
         </div>
     )
 }
